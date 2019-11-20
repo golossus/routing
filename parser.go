20efa642c6bf9002bdc6d4b9ce4ae7c861776376
David Camarena <david.camarena@xing.com>
2019-11-20 10:46:39 +0100

    working on the lexer

--- parser.go
+++ parser.go
@@ -2,6 +2,7 @@ package hw14_go
 
 import (
 	"bufio"
+	"bytes"
 	"strings"
 )
 
@@ -14,6 +15,12 @@ const (
 	TFinishVar
 	TEnd
 )
+
+const (
+	TModeStatic = iota
+	TModeIdentifier
+)
+
 
 type token struct {
 	v string
@@ -48,11 +55,115 @@ func createEndToken() token {
 	return token{t: TEnd, v: ""}
 }
 
+
+func isSlash(ch rune) bool {
+	return '/' == ch
+}
+
+func isOpenBrace(ch rune) bool {
+	return '{' == ch
+}
+
+func isCloseBrace(ch rune) bool {
+	return '}' == ch
+}
+
+func isAlpha(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') ||
+		(ch >= 'A' && ch <= 'Z') ||
+		(ch >= '0' && ch <= '9')
+}
+func isStatic(ch rune) bool {
+	return isAlpha(ch) ||
+		(ch == '.') ||
+		(ch == '-') ||
+		(ch == '_')
+}
+
 type Lexer struct {
+	mode int
 	buf *bufio.Reader
 }
 
 func NewLexer(path string) *Lexer {
 	reader := strings.NewReader(path)
-	return Lexer{buf: bufio.Reader{rd: reader}}
+	return &Lexer{buf: bufio.NewReader(reader), mode: TModeStatic}
 }
+
+func (l *Lexer) scan() token {
+	ch, _, err := l.buf.ReadRune()
+
+	if nil != err {
+		return createEndToken()
+	}
+
+	if l.mode == TModeIdentifier {
+
+		if (isCloseBrace(ch)) {
+			l.mode = TModeStatic
+			return createCloseVarToken()
+		}
+
+		return l.scanIdentifier()
+
+	}
+
+	// defaults to Static mode
+
+	if isSlash(ch) {
+		return createSlashToken()
+	}
+
+	if isOpenBrace(ch) {
+		l.mode = TModeIdentifier
+		return createOpenVarToken()
+	}
+
+	return l.scanStatic()
+
+}
+
+func (l *Lexer) scanStatic() token {
+	var out bytes.Buffer
+
+	for {
+		ch, _, err := l.buf.ReadRune()
+
+		if nil != err {
+			break
+		}
+
+		if !isStatic(ch) {
+			l.buf.UnreadRune()
+			break
+		}
+
+		out.WriteRune(ch)
+	}
+
+	return createStaticToken(out.String())
+}
+
+func (l *Lexer) scanIdentifier() token {
+	var out bytes.Buffer
+
+	for {
+		ch, _, err := l.buf.ReadRune()
+
+		if nil != err {
+			break
+		}
+
+		if !isAlpha(ch) {
+			l.buf.UnreadRune()
+			break
+		}
+
+		// is valid
+
+		out.WriteRune(ch)
+	}
+
+	return createVarToken(out.String())
+}
+
