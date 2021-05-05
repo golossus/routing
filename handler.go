8925ce710b03f20db7ea094c08850bc4264d97fe
Jorge Brisa <jorge.br.ib@gmail.com>
2021-05-05 11:51:19 +0200

    adds redirect method to external or internal path (#36)
    Co-authored-by: Santiago Garcia <sangarbe@gmail.com>

--- handler.go
+++ handler.go
@@ -15,6 +15,16 @@ func getAutoMethodOptionsHandler(router *Router) http.HandlerFunc {
 	}
 }
 
+func getRedirectHandler(url string, code ...int) http.HandlerFunc {
+	defaultCode := http.StatusFound
+	if len(code) > 0 && code[0] >= http.StatusMultipleChoices && code[0] <= http.StatusPermanentRedirect {
+		defaultCode = code[0]
+	}
+	return func(writer http.ResponseWriter, request *http.Request, ) {
+		http.Redirect(writer, request, url, defaultCode)
+	}
+}
+
 func getAvailableMethods(router *Router, request *http.Request) []string {
 	availVerbs := make([]string, 0, 9)
 	for verb, tree := range router.trees {
